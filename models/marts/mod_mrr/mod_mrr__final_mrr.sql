28af1e7167a24a67e8fb0acb9e6537febadd9ff6
Jill Zotter <jzott155@gmail.com>
2024-02-28 19:29:14 +0000

    updating a bunch of files including snowflake spending

--- models/marts/mod_mrr/mod_mrr__final_mrr.sql
+++ models/marts/mod_mrr/mod_mrr__final_mrr.sql
@@ -35,7 +35,7 @@ mrr_with_changes as (
 final as (
 
     select
-        {# {{  dbt_utils.generate_surrogate_key('date_month', 'customer_id') }} as id, #}
+         {{  dbt_utils.generate_surrogate_key(['date_month', 'customer_id']) }} as id,
 
         *,
 
